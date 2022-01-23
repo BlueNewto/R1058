3886fdcfde4204cc9260fe08f5d5d9f53a98cb49
R1058 <97218351+R1058@users.noreply.github.com>
2022-01-23 10:45:37 +0530

    cc

--- httpd_start.sh
+++ httpd_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo systemctl start nginx
 sudo systemctl status nginx
-cd /opt/codedeploy-agent/deployment-root/620eb327-dbb4-477c-893e-296902704276/d-*/deployment-archive
+cd /opt/codedeploy-agent/deployment-root/c42*/d-*/deployment-archive
 sudo rm -rf node_modules
 sudo npm install
-sh /opt/codedeploy-agent/deployment-root/620eb327-dbb4-477c-893e-296902704276/d-*/deployment-archive/execute.sh
+sh /opt/codedeploy-agent/deployment-root/c42*/d-*/deployment-archive/execute.sh
